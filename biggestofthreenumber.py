07258e54047c42de1292df59843d1459181fd6dd
ajzell18 <muhammedajzel18@gmail.com>
2025-11-12 15:59:46 +0530

    Add files via upload

---
+++ biggestofthreenumber.py
@@ -0,0 +1,7 @@
+a = int(input("Enter A: "))
+b = int(input("Enter B: "))
+c = int(input("Enter C: "))
+big = a
+if b > big: big = b
+if c > big: big = c
+print(big)
---
+++ positivelista.py
@@ -0,0 +1,6 @@
+number=[-2,5,-8,9,0,3,-1]
+positive_list=[]
+for num in number:
+    if num>0:
+        positive_list.append(num)
+        print("positive numbers:",positive_list)
